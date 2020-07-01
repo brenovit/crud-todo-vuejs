62a28447dbd0f784cf0ccac9740af8a31504c581
Breno Nunes <breno.arantes@wexinc.com>
2020-06-30 23:49:49 -0300

    Enable cors

--- src/http-common.js
+++ src/http-common.js
@@ -4,5 +4,9 @@ export default axios.create({
   baseURL: 'https://todo-crud-node.herokuapp.com/api',
   headers: {
     'Content-type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, DELETE, PUT',
+    'Access-Control-Allow-Headers':
+      'X-Requested-With, Content-Type, X-Codingpedia',
   },
 });
